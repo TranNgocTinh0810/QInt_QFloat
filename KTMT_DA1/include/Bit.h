a71dbcfb4e83d3888b19f184dc6a708a4e15c211
TinhTran <55842203+TranNgocTinh0810@users.noreply.github.com>
2020-05-04 15:44:42 +0700

    Update Bit.h

---
+++ KTMT_DA1/include/Bit.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+class Bit
+{
+private:
+	bool m_bit;
+public:
+	Bit();
+	Bit(bool bit);
+	Bit(const Bit& bit);
+	~Bit();
+
+	Bit& operator=(const Bit& bit);
+	bool operator==(const Bit& bit)const;
+	bool operator!=(const Bit& bit)const;
+	Bit operator|(const Bit& bit);
+	Bit operator&(const Bit& bit);
+	Bit operator^(const Bit& bit);
+	Bit operator~();
+
+	bool isBit0()const;
+	bool isBit1()const;
+};
